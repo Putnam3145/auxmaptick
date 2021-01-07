160c63d80cd80afebd6d92170c063cbecfa9da53
Putnam <putnam3145@gmail.com>
2021-01-06 23:53:13 -0800

    Okay no this is how it works.

--- README.md
+++ README.md
@@ -1,3 +1,19 @@
 Adds functionality to [auxtools](https://github.com/willox/auxtools) that sets a global variable in byond whenever it sends maps showing how long sending maps took, in ticks.
 
-To use, define a `/proc/initialize_maptick` and call it in `/world/New`.
+To use, compile this for your target OS, put the DLL into the same folder as your .dme, then:
+
+1. Define a macro or function to enable the DLL's instance of auxtools, e.g.
+```c
+#define AUXMAPTICK ((world.system_type == MS_WINDOWS ? "auxmaptick.dll" : "auxmaptick.so"))
+#define AUXMAPTICK_CHECK\
+	if (!GLOB.auxmaptick_initialized && fexists(AUXMAPTICK) && findtext(call(AUXMAPTICK,"auxtools_init")(),"SUCCESS"))\
+		GLOB.auxmaptick_initialized = TRUE;\
+```
+2. Define a `/proc/initialize_maptick` and call it in `/world/New`.
+3. Do as 1, but for disabling it, e.g.
+```c
+#define AUXMAPTICK_SHUTDOWN\
+	if (GLOB.auxmaptick_initialized && fexists(AUXMAPTICK))\
+		call(AUXMAPTICK,"auxtools_shutdown")();\
+		GLOB.auxmaptick_initialized = FALSE;\
+```
--- src/lib.rs
+++ src/lib.rs
@@ -4,50 +4,56 @@ use detour::RawDetour;
 
 use std::ffi::c_void;
 
-extern "C" {
-    static mut send_maps_original: unsafe extern "C" fn();
+thread_local! {
+    static SEND_MAPS_ORIGINAL: unsafe extern "C" fn() = {
+        let byondcore = sigscan::Scanner::for_module(BYONDCORE).unwrap();
+        let mut send_maps_byond: *const c_void = std::ptr::null();
+        if cfg!(windows) {
+            let ptr = byondcore
+                .find(signature!(
+                    "55 8B EC 6A FF 68 ?? ?? ?? ?? ?? ?? ?? ?? ?? ?? 50 81 EC ?? ?? ?? ?? A1 ?? ?? ?? ?? 33 C5 89 45 F0 53 56 57 50 8D 45 F4 ?? ?? ?? ?? ?? ?? A0 ?? ?? ?? ?? 04 01 75 05 E8 ?? ?? ?? ?? E8"
+                ))
+                .unwrap();
+    
+            send_maps_byond = ptr as *const c_void;
+        }
+    
+        if cfg!(unix) {
+            let ptr = byondcore
+                .find(signature!(
+                    "55 89 E5 57 56 53 81 EC ?? ?? ?? ?? 80 3D ?? ?? ?? ?? ?? 0F 84 ?? ?? ?? ??"
+                ))
+                .unwrap();
+    
+            send_maps_byond = ptr as *const c_void;
+        }
+        unsafe {
+            let tick_hook = RawDetour::new(
+                send_maps_byond as *const (),
+                map_tick_hook as *const (),
+            )
+            .unwrap();
+        
+            tick_hook.enable().unwrap();
+            let ret = std::mem::transmute(tick_hook.trampoline());
+            std::mem::forget(tick_hook);
+            ret
+        }
+    }
 }
 
 #[no_mangle]
 unsafe extern "C" fn map_tick_hook() {
     let start = std::time::Instant::now();
-    send_maps_original();
+    SEND_MAPS_ORIGINAL.with(|send_maps_original| {
+        send_maps_original();
+    });
 	Value::globals().set("internal_tick_usage", start.elapsed().as_micros() as f32 / 100000.0);
 }
 
 #[hook("/proc/initialize_maptick")]
-fn _init_maptick_hook() {
-    let byondcore = sigscan::Scanner::for_module(BYONDCORE).unwrap();
-    let mut send_maps_byond: *const c_void = std::ptr::null();
-	if cfg!(windows) {
-		let ptr = byondcore
-			.find(signature!(
-				"55 8B EC 6A FF 68 ?? ?? ?? ?? ?? ?? ?? ?? ?? ?? 50 81 EC ?? ?? ?? ?? A1 ?? ?? ?? ?? 33 C5 89 45 F0 53 56 57 50 8D 45 F4 ?? ?? ?? ?? ?? ?? A0 ?? ?? ?? ?? 04 01 75 05 E8 ?? ?? ?? ?? E8"
-			))
-			.ok_or_else(|| runtime!("Couldn't find send_maps"))?;
-
-        send_maps_byond = ptr as *const c_void;
-	}
-
-	if cfg!(unix) {
-		let ptr = byondcore
-			.find(signature!(
-				"55 89 E5 57 56 53 81 EC ?? ?? ?? ?? 80 3D ?? ?? ?? ?? ?? 0F 84 ?? ?? ?? ??"
-			))
-			.ok_or_else(|| runtime!("Couldn't find send_maps"))?;
-
-        send_maps_byond = ptr as *const c_void;
-    }
-    unsafe {
-        let tick_hook = RawDetour::new(
-            send_maps_byond as *const (),
-            map_tick_hook as *const (),
-        )
-        .map_err(|_| runtime!("Couldn't detour send_maps"))?;
-    
-        tick_hook.enable().map_err(|_| runtime!("Couldn't enable send_maps detour"))?;
-        send_maps_original = std::mem::transmute(tick_hook.trampoline());
-        std::mem::forget(tick_hook);
-    };
-    Ok(Value::from(true))
+fn _init_map_tick() {
+    Ok(Value::from(SEND_MAPS_ORIGINAL.with(|_| {
+        true
+    })))
 }
